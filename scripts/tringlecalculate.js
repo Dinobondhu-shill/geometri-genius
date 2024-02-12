c4f008fb664a276a389fb6a4b1c1add6afd6198d
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-02-13 01:09:22 +0600

    triangle area calculation logic added

--- scripts/tringlecalculate.js
+++ scripts/tringlecalculate.js
@@ -1,3 +1,16 @@
 function TriangleCalculation (){
-  
+  // get the base from Input
+  const triangleBase = document.getElementById('triangle-base');
+  const triangleBaseText = triangleBase.value;
+  const base = parseFloat (triangleBaseText);
+// get the height from input
+  const triangleHeight = document.getElementById('triangle-height');
+  const triangleHeightText = triangleHeight.value;
+  const height = parseFloat (triangleHeightText);
+// Calculation the Area by using Logic
+const Area = 0.5*base*height;
+
+// Display the output
+const triangleArea = document.getElementById('triangle-area');
+triangleArea.innerText = Area;
 }
